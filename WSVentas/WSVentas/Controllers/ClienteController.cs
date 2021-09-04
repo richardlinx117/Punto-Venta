8a21c338fe4c4e177cdfeda0e6de7de871ab0a01
richardlinx117 <54791684+richardlinx117@users.noreply.github.com>
2021-09-04 09:41:26 -0500

    Add files via upload
    Esta es la api realizada en .Net

---
+++ WSVentas/WSVentas/Controllers/ClienteController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSVentas.Models;
+
+namespace WSVentas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using(VentaContext db = new VentaContext())
+            {
+                var lista = db.Clientes.ToList();
+                return Ok(lista);
+            }
+            
+        }
+    }
+}
---
+++ WSVentas/WSVentas/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSVentas.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            List<WeatherForecast> lista = new List<WeatherForecast>();
+            lista.Add(new WeatherForecast() { Id = 2, Nombre = "Ricardo" });
+            lista.Add(new WeatherForecast() { Id = 5, Nombre = "Sarahy" });
+
+            return lista;
+            
+        }
+    }
+}
